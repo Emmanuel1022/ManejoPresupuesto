f1949e9679075d9e1ff35a56e2f5d2176cf7f71e
Emmanuel1022 <78395166+Emmanuel1022@users.noreply.github.com>
2022-08-24 22:09:35 -0600

    Se agrega el proyecto

---
+++ Controllers/TiposCuentasController.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController: Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+
+        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas)
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+        }
+        public IActionResult Crear()
+        {
+
+      
+                return View();
+        
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipocuenta)
+        {
+
+
+            if (!ModelState.IsValid)
+            {
+                return View(tipocuenta);
+
+            }
+
+            repositorioTiposCuentas.Crear(tipocuenta);
+
+            return View();        
+        
+        }
+    }
+}
